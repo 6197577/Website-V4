d9fbb81134bd4d5675c44bb2c035eedd85bf0cd3
Doc <admin@electricdrs.com>
2025-08-23 03:31:40 -0400

    update _app.js

--- _app.js
+++ _app.js
@@ -41,7 +41,7 @@ export default function Application({ Component, pageProps }) {
 
     return <>
         <Head>
-            <title>Electric Doctors: Alleviating your electrical pains!</title>
+            <title>Electric Doctors: We have your cure 24-7</title>
             <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0" />
         </Head>
         <Script
